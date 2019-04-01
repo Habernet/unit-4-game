93f4148eb86160669a02347cde1763691d7abd3a
Patrick Haberern <patrick@Patricks-MBP.nc.rr.com>
2019-04-01 14:11:14 -0400

    Added a basic bootstrap-html structure. Created objects representing each character. 	Created a few variables to grab elements off the page for display later. 	Linked Bootstrap and Jquery.

---
+++ assets/javascript/game.js
@@ -0,0 +1,39 @@
+
+
+// Objects representing the characters
+var masterChief = {
+    healthpoints: 200,
+    attack: 5,
+    counterattack: 8,
+    increaseAttack: function(){
+        this.attack += this.attack;
+    }
+}
+
+var arbiter = {
+    healthpoints: 150,
+    attack: 7,
+    counterattack: 8,
+    increaseAttack: function(){
+        this.attack += this.attack;
+    }
+}
+
+var majorJohnson = {
+    healthpoints: 130,
+    attack: 10,
+    counterattack: 12,
+    increaseAttack: function(){
+        this.attack += this.attack;
+    }
+}
+
+var tartarus = {
+    healthpoints: 180,
+    attack: 8,
+    counterattack: 7.5,
+    increaseAttack: function(){
+        this.attack += this.attack;
+    }
+}
+
